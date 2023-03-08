16ef869feaf24432e4219ebe39707270167202ce
kyaramero <julia.b.andrade@outlook.com>
2023-03-08 12:33:57 -0300

    Feature(create-user): Created interface and made data appear in the console

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
 import { Login } from './scripts/pages/login.page.js'
 import { NotFound } from './scripts/pages/not-found.page.js'
+import { CreateUser } from './scripts/pages/sign-up.page.js'
 
 function redirectPage() {
-    const root = document.querySelector('#root')
+  const root = document.querySelector('#root')
 
-    const Router = {
-        "": { component: Login, path: "#login" },
-        "#login": { component: Login, path: "#login" },
-        "#404": { component: NotFound, path: "#404" },
-    }
+  const Router = {
+    '': { component: Login, path: '#login' },
+    '#login': { component: Login, path: '#login' },
+    '#404': { component: NotFound, path: '#404' },
+    '#createUser': { component: CreateUser, path: '#createUser' },
+  }
 
-    let route = Router[window.location.hash]
+  let route = Router[window.location.hash]
 
-    if(!route) {
-        route = Router['#404']
-        window.location.href = route.path
-    }
+  if (!route) {
+    route = Router['#404']
+    window.location.href = route.path
+  }
 
-    root.innerHTML = null
-    root.append(route.component())
+  root.innerHTML = null
+  root.append(route.component())
 }
 
 window.addEventListener('load', () => {
-    redirectPage()
-    window.addEventListener('hashchange', redirectPage)
+  redirectPage()
+  window.addEventListener('hashchange', redirectPage)
 })
